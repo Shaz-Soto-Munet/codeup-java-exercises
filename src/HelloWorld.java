ece95ba24ec32ecb1d64ddd58c69acf94d8f32e0
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-24 16:28:39 -0500

    Finalize first two java exercises.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+        int myFavoriteNumber = 28;
+        System.out.println("My favorite number is: " + myFavoriteNumber);
+
+        String myString = "This is a string, bing bong!";
+        System.out.println(myString);
+
+        myString = "This is a new string, bing bawng!";
+        System.out.println(myString);
+
+//        myString = 3.14159;
+//        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        System.out.println(Integer.MAX_VALUE);
+
     }
 }
---
+++ src/YoHey.java
@@ -0,0 +1,12 @@
+public class YoHey {
+    public static void main(String[] args) {
+        System.out.println("Yo, hey!");
+        System.out.println("Hello Shaz");
+        System.out.println("Is this working?");
+
+        int myFavoriteNumber = 28;
+        System.out.println("My favorite number is: " + myFavoriteNumber);
+
+
+    }
+}
