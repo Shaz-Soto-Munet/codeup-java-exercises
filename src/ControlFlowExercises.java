31e5fa9068bdfc7f7282a1ce1d8e4142b24ac037
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-28 16:16:04 -0500

    Finish exercise 2 in ControlFlowExercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -17,5 +17,19 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+        for (int i=1; i<=100; i++){
+            if (i%3==0 & i%5==0){
+                System.out.println("FizzBuzz");
+                continue;
+            } else if (i%3==0){
+                System.out.println("Fizz");
+                continue;
+            } else if (i%5==0){
+                System.out.println("Buzz");
+                continue;
+            }
+            System.out.println(i);
+        }
+
     }
 }
