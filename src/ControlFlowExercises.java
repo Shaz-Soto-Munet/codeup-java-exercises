3bb1332fec8af1603cd26e145ab5a172ffc8af08
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-28 17:00:39 -0500

    Make progress on last exercise in ControlFlowExercises class.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        int i = 5;
@@ -17,18 +19,49 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for (int i=1; i<=100; i++){
-            if (i%3==0 & i%5==0){
-                System.out.println("FizzBuzz");
-                continue;
-            } else if (i%3==0){
-                System.out.println("Fizz");
-                continue;
-            } else if (i%5==0){
-                System.out.println("Buzz");
-                continue;
+//        for (int i=1; i<=100; i++){
+//            if (i%3==0 & i%5==0){
+//                System.out.println("FizzBuzz");
+//                continue;
+//            } else if (i%3==0){
+//                System.out.println("Fizz");
+//                continue;
+//            } else if (i%5==0){
+//                System.out.println("Buzz");
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What number would you like to go up to?");
+        int userInt = sc.nextInt();
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i=1; i<=userInt; i++){
+            int squared = (int) Math.pow(i,2);
+            int cubed = (int) Math.pow(i,3);
+            System.out.println(i + "      | " + squared + "       | " + cubed);
+        }
+        System.out.println("Do you wish to continue?");
+        String userAnswer = sc.next();
+        if (userAnswer.equals("yes")){
+            System.out.println("Please enter a number between 0 and 100.");
+            int userNumber = sc.nextInt();
+            switch (userNumber) {
+                case userNumber - 88 >= 0:
+                    System.out.println("A");
+                    break;
+                case userNumber - 80 >= 0:
+                    System.out.println("B");
+                    break;
+                default:
+                    System.out.println("Ya dun goof'd");
             }
-            System.out.println(i);
+
+        } else {
+            System.out.println("You chose not to continue.");
         }
 
     }
