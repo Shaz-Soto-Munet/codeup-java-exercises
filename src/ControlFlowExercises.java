c88d6125b7311eb01c85ae94de538c2142d300c7
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-28 16:06:49 -0500

    Add ControlFlowExercises java class.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,21 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i<=15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i = (int) Math.pow(i,2);
+//        } while (i < 1000000);
+
+//        for (int i=2; i<1000000; i = (int) Math.pow(i,2)){
+//            System.out.println(i);
+//        }
+
+    }
+}
