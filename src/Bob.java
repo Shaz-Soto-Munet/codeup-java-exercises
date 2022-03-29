a0172a32c5997d17f0d53627ff7a602448837d3b
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-29 15:52:54 -0500

    Add Bob.

---
+++ src/Bob.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This is Bob, the lackadaisical teenager. Say hi!");
+
+        do {
+            String userInput = "";
+            userInput = scanner.nextLine();
+
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever...");
+            }
+        } while (true);
+    }
+}
