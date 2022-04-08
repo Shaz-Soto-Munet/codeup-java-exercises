4b6aa889dcd7b27cf1ca9dafdf632288574b97b4
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-04-08 11:23:10 -0500

    Add FileIO Lecture files.

---
+++ src/fileio/FileIOLectureExercise.java
@@ -0,0 +1,22 @@
+package fileio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileIOLectureExercise {
+    public static void main(String[] args) {
+        String myPath = "/Users/files/this.text";
+        String fileName = "dishes.txt";
+        String directory = "data";
+
+//        Path filePath = Paths.get(directory,fileName);
+//
+//        System.out.println("This is your path: " + filePath.toAbsolutePath());
+//
+//        Path absolutePath = Paths.get("/", "user");
+//
+//        System.out.println("absolutePath.isAbsolute() = " + absolutePath.isAbsolute());
+
+
+    }
+}
